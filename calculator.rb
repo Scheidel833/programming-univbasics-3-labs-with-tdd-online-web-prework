42e7728575a2bda8311263f94ea5dabd31b7328b
Tina F. Watkins <watkinst27@yahoo.com>
2019-07-13 15:40:27 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
-# Add your variables here
+#Add your variables here
+first_number = 2
+second_number = 3
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+ qoutient = first_number / second_number
+ 
